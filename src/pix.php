a221191f516e148190a7070c05e481a34bcc58c5
Israel Nogueira <israel@feats.com.br>
2025-07-24 13:10:06 -0300

    UPDATE

--- src/pix.php
+++ src/pix.php
@@ -167,6 +167,28 @@ trait pix{
 		} catch (\Throwable $ERROR) {throw new Exception($ERROR->getMessage());}}
 
 
+		static public function simularPagamentoPIX($qrCodeId=null){
+			try {
+				self::verifyEnv();
+				if(is_null($qrCodeId)) throw new Exception("qrCodeId não informado");
+
+				$ch = curl_init();
+				curl_setopt($ch, CURLOPT_URL, getEnv(getEnv('ASAAS_AMBIENTE')) . "/v3/sandbox/pix/qrCodes/{$qrCodeId}/pay");
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_HEADER, false);
+				curl_setopt($ch, CURLOPT_POST, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([]));
+				curl_setopt($ch, CURLOPT_HTTPHEADER, [
+					"Content-Type: application/json",
+					"access_token: " . getEnv(getEnv('ASAAS_APIKEY'))
+				]);
+				$response = curl_exec($ch);
+				curl_close($ch);
+				return json_decode($response, true);
+			} catch (\Throwable $ERROR) {
+				throw new Exception($ERROR->getMessage());
+			}
+		}
 
 
 }
--- src/sandBox.php
+++ src/sandBox.php
@@ -5,7 +5,7 @@ use Exception;
 //⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮ CARTÃO DE CREDITO ⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮
 //#####################################################################################
 trait sandBox{
-	
+
 	static public function confirmPayment($_ID){
 		try{
 			self::verifyEnv();
@@ -49,5 +49,31 @@ trait sandBox{
 			throw new Exception($ERROR->getMessage());
 		}
 	}
+
+	static public function pagaPIX($qrCodeId=null){
+		try {
+			self::verifyEnv();
+			if(is_null($qrCodeId)) throw new Exception("qrCodeId não informado");
+
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, getEnv(getEnv('ASAAS_AMBIENTE')) . "/v3/sandbox/pix/qrCodes/{$qrCodeId}/pay");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HEADER, false);
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([]));
+			curl_setopt($ch, CURLOPT_HTTPHEADER, [
+				"Content-Type: application/json",
+				"access_token: " . getEnv(getEnv('ASAAS_APIKEY'))
+			]);
+			$response = curl_exec($ch);
+			curl_close($ch);
+			return json_decode($response, true);
+		} catch (\Throwable $ERROR) {
+			throw new Exception($ERROR->getMessage());
+		}
+	}
+
+
+
 
 }
